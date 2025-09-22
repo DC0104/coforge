3767237a51ec001a50b81ad29b95d6a5fe857b00
Dhruv Choudhury <dhruvchoudhury2003@gmail.com>
2025-09-22 16:46:39 +0000

    comm

---
+++ models/Stage/stg_customer.sql
@@ -0,0 +1,12 @@
+with customer as (
+select
+    c_name as name,
+    c_address as address,
+    c_phone as phone_number,
+    c_acctbal as account_balance,
+    c_mktsegment as market_segment,
+    c_comment as c_comment
+from customer
+)
+
+select * from customer
