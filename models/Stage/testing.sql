01c34f3835de9966ad563cbcac36f3e411bd9300
Dhruv Choudhury <dhruvchoudhury2003@gmail.com>
2025-09-23 10:05:29 +0000

    c

--- models/Stage/testing.sql
+++
@@ -1,5 +0,0 @@
-select max(update_time)from (
-        select * from {{ this }}
-        where update_time > dateadd(day,-7,current_date)
-        qualify count (*) over (partition by updated_time::date)>2000
-          
