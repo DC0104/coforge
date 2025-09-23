3dca03bd7416d4aa8cd47940c9f91e83bede4b36
Dhruv Choudhury <dhruvchoudhury2003@gmail.com>
2025-09-23 05:16:17 +0000

    c

---
+++ models/Stage/stg_supplier.sql
@@ -0,0 +1,14 @@
+with supplier as (
+select
+    s_suppkey as s_suppkey,
+    s_name as name,
+    s_address as address,
+    s_nationkey as nationkey,
+    s_phone as phone,
+    s_acctbal as acctbal,
+    s_comment as comment,
+    updated_time as updated_time
+from {{ source('src','suppliers') }}
+)
+
+select * from supplier
