bf1aebf202b7d8da55cc442cc73029090f33cdc6
Dhruv Choudhury <dhruvchoudhury2003@gmail.com>
2025-09-24 06:56:11 +0000

    c

--- models/Stage/stg_order.sql
+++ models/Stage/stg_order.sql
@@ -2,12 +2,12 @@ with order as (
 select
     o_orderkey as orderkey,
     o_custkey as custkey,
-    o_orderstatus as orderstatus,
-    o_totalprice as totalprice,
-    o_orderdate as orderdate,
-    o_orderpriority as orderpriority,
+    o_orderstatus as order_status,
+    o_totalprice as total_price,
+    o_orderdate as order_date,
+    o_orderpriority as order_priority,
     o_clerk as clerk,
-    o_shippriority as shippriority,
+    o_shippriority as ship_priority,
     o_comment as comment
 from {{ source('src','orders') }}
 )
