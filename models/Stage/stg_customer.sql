ea8168e5bb6c7ca44bf33b6df1507c12f492e2a3
Dhruv Choudhury <dhruvchoudhury2003@gmail.com>
2025-09-24 05:33:21 +0000

    c

--- models/Stage/stg_customer.sql
+++ models/Stage/stg_customer.sql
@@ -1,11 +1,12 @@
 with customer as (
 select
+    c_custkey as custkey,
     c_name as name,
     c_address as address,
     c_phone as phone_number,
     c_acctbal as account_balance,
     c_mktsegment as market_segment,
-    c_comment as c_comment
+    c_comment as comment
 from {{ source('src','customers') }}
 )
 
