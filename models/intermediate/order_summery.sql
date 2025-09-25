ffa269d38773cebebf472295d2dfa7eab6bbbc1e
Dhruv Choudhury <dhruvchoudhury2003@gmail.com>
2025-09-25 06:21:14 +0000

    c

--- models/intermediate/order_summery.sql
+++ models/intermediate/order_summery.sql
@@ -1,9 +1,29 @@
+-- with orders as (
+--     select * from {{source('src','orders')}}
+-- ),
+-- final as (
+--     select 
+--     o_orderstatus order_status,
+--     sum(case when o_orderpriority='1-URGENT' then o_totalprice else 0 end)
+--         as URGENT,
+--     sum(case when o_orderpriority='2-HIGH' then o_totalprice else 0 end)
+--         as HIGH,
+--     sum(case when o_orderpriority='3-MEDIUM' then o_totalprice else 0 end)
+--         as MEDIUM,
+--     sum(case when o_orderpriority='4-LOW' then o_totalprice else 0 end)
+--         as LOW
+-- FROM orders
+-- group by 1
+-- )
+
+-- select * from final
+
 with orders as (
-    select * from {{source('src','orders')}}
-)
+    select * from {{ref('stg_order')}}
+),
 final as (
     select 
-    o_orderstatus order_status,
+    order_status,
     sum(case when order_priority='1-URGENT' then total_price else 0 end)
         as URGENT,
     sum(case when order_priority='2-HIGH' then total_price else 0 end)
